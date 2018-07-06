cf11d8ba431e962d8085abe60ebc8d2fc9ede16a
zyycj <wmzt.zj@gmail.com>
2018-07-06 17:38:51 +0000

    finish lock

--- lock_server.cc
+++ lock_server.cc
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <stdio.h>
 
 lock_server::lock_server():
   nacquire (0)
@@ -19,5 +20,41 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   r = nacquire;
   return ret;
 }
+
+lock_protocol::status 
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r){
+    lock_protocol::status ret = lock_protocol::OK;
+    r = 0; // r doesn't matter for result;
+    printf("release from clt %d, lid %llu\n", clt, lid);
+    pthread_mutex_lock(&mtx);
+    if(lock_table.find(lid) != lock_table.end()) {
+        printf("lid %llu notify\n", lid);
+        lock_table.erase(lid);
+        pthread_cond_signal(&cond);    
+        
+    }
+    pthread_mutex_unlock(&mtx);
+    return ret;
+}
+
+lock_protocol::status 
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r){
+
+    lock_protocol::status ret = lock_protocol::OK;
+    r = 0; // r doesn't matter for result;
+    printf("acuire from clt %d, lid %llu\n", clt, lid);
+    pthread_mutex_lock(&mtx);
+    while(lock_table.find(lid) != lock_table.end()) {
+        printf("lid %llu start wait\n", lid);
+        pthread_cond_wait(&cond, &mtx);
+    }
+
+    lock_table[lid] = 1;
+    printf("lid %llu get lock\n", lid);
+    
+    pthread_mutex_unlock(&mtx);
+    return ret;
+}
+
 
 
--- lock_server.h
+++ lock_server.h
@@ -5,6 +5,8 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
@@ -13,11 +15,17 @@ class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, int> lock_table;
+  pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+  pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
 
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t, int &);
 };
 
 #endif 
