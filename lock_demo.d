fc53afaaa8793da5031ccabaf1aef42d9142a1e0
threezj <threezj@hotmail.com>
2018-08-14 05:18:27 -0400

    pass lab1

---
+++ lock_demo.d
@@ -0,0 +1,3 @@
+lock_demo.o: lock_demo.cc lock_protocol.h rpc/rpc.h rpc/connection.h \
+ rpc/pollmgr.h rpc/marshall.h lang/verify.h lang/algorithm.h \
+ rpc/thr_pool.h rpc/fifo.h rpc/slock.h lock_client.h
--- lock_server.cc
+++ lock_server.cc
@@ -1,60 +1,49 @@
 // the lock server implementation
 
 #include "lock_server.h"
-#include <sstream>
-#include <stdio.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sstream>
 
-lock_server::lock_server():
-  nacquire (0)
-{
-}
+lock_server::lock_server() : nacquire(0) {}
 
-lock_protocol::status
-lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
-{
-  lock_protocol::status ret = lock_protocol::OK;
-  printf("stat request from clt %d\n", clt);
-  r = nacquire;
-  return ret;
-}
-
-lock_protocol::status 
-lock_server::release(int clt, lock_protocol::lockid_t lid, int &r){
+lock_protocol::status lock_server::stat(int clt, lock_protocol::lockid_t lid,
+                                        int &r) {
     lock_protocol::status ret = lock_protocol::OK;
-    r = 0; // r doesn't matter for result;
-    printf("release from clt %d, lid %llu\n", clt, lid);
-    pthread_mutex_lock(&mtx);
-    if(lock_table.find(lid) != lock_table.end()) {
-        printf("lid %llu notify\n", lid);
-        lock_table.erase(lid);
-        pthread_cond_signal(&cond);    
-        
-    }
-    pthread_mutex_unlock(&mtx);
+    printf("stat request from clt %d\n", clt);
+    r = nacquire;
     return ret;
 }
 
-lock_protocol::status 
-lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r){
-
+lock_protocol::status lock_server::release(int clt, lock_protocol::lockid_t lid,
+                                           int &r) {
     lock_protocol::status ret = lock_protocol::OK;
-    r = 0; // r doesn't matter for result;
-    printf("acuire from clt %d, lid %llu\n", clt, lid);
+    r = 0;  // r doesn't matter for result;
+    //printf("release from clt %d, lid %llu\n", clt, lid);
     pthread_mutex_lock(&mtx);
-    while(lock_table.find(lid) != lock_table.end()) {
-        printf("lid %llu start wait\n", lid);
+    if (lock_table.find(lid) != lock_table.end()) {
+        lock_table.erase(lid);
+        pthread_cond_signal(&cond);
+    }
+    pthread_mutex_unlock(&mtx);
+    //printf("release from clt %d, lid %llu succeed\n", clt, lid);
+    return ret;
+}
+
+lock_protocol::status lock_server::acquire(int clt, lock_protocol::lockid_t lid,
+                                           int &r) {
+    lock_protocol::status ret = lock_protocol::OK;
+    r = 0;  // r doesn't matter for result;
+    //printf("acuire from clt %d, lid %llu\n", clt, lid);
+    pthread_mutex_lock(&mtx);
+    while (lock_table.find(lid) != lock_table.end()) {
         pthread_cond_wait(&cond, &mtx);
     }
 
     lock_table[lid] = 1;
-    printf("lid %llu get lock\n", lid);
-    
     pthread_mutex_unlock(&mtx);
+    //printf("clt %d, get lock lid %llu\n", clt, lid);
     return ret;
 }
-
-
-
