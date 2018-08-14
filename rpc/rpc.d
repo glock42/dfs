6484bc36d3937226522d9a5d9ecef13e3d277598
threezj <threezj@hotmail.com>
2018-08-14 05:18:56 -0400

    del obj

--- rpc/rpc.d
+++
@@ -1,3 +0,0 @@
-rpc/rpc.o: rpc/rpc.cc rpc/rpc.h rpc/connection.h rpc/pollmgr.h \
- rpc/marshall.h lang/verify.h lang/algorithm.h rpc/thr_pool.h rpc/fifo.h \
- rpc/slock.h rpc/method_thread.h gettime.h rpc/jsl_log.h
